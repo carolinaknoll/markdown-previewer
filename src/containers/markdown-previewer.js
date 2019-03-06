09a206c59781b83268034587332697c8947f3a96
Carolina Knoll <carolinasknoll@gmail.com>
2019-03-06 20:40:55 -0300

    Add support for nightly/brightly theme switching, fix footer spacing issue

--- src/containers/markdown-previewer.js
+++ src/containers/markdown-previewer.js
@@ -4,11 +4,31 @@ import Header from '../components/common/header';
 import Footer from '../components/common/footer';
 
 export default class MarkdownPreviewer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isNightlyTheme: true,
+    }
+  }
+
+  toggleTheme = (event) => {
+    this.setState({isNightlyTheme: !this.state.isNightlyTheme});
+  }
+
   render() {
+    const {isNightlyTheme} = this.state;
+
     return (
-      <div className="nightly-theme">
+      <div className={isNightlyTheme ? 'nightly-theme' : 'brightly-theme'}>
         <div className="content-container">
           <Header />
+
+          <h3 className="toggle-theme" onClick={this.toggleTheme}>
+            Switch to {isNightlyTheme ? 'brightly' : 'nightly'} theme
+            <i className={isNightlyTheme ? 'fa fa-sun-o' : 'fa fa-moon-o'}></i>
+          </h3>
+
           <UserInput />
         </div>
 
