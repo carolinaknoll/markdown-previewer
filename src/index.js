701c72a0f167ac583cf1c826f62cd5de45836e85
Carolina Knoll <carolinasknoll@gmail.com>
2019-08-20 08:30:09 -0300

    Remove unneeded react-router-dom dependency

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter, Route, Switch} from 'react-router-dom';
-
+import MarkdownPreviewer from './containers/markdown-previewer';
 import './index.css';
 
-import MarkdownPreviewer from './containers/markdown-previewer';
-
 ReactDOM.render(
-  <HashRouter>
-    <Route exact path='/' component={MarkdownPreviewer} />
-  </HashRouter>,
+  <MarkdownPreviewer />,
   document.getElementById('root'),
 );
